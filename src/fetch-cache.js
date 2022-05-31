3333ec7877ea6a1ddaef3bd429caa7337f6172f1
James <james@jamesnzl.xyz>
2022-05-31 19:04:59 +1200

    feat: :loud_sound: add more logging

--- src/fetch-cache.js
+++ src/fetch-cache.js
@@ -5,17 +5,24 @@ export async function fetchCache(opts) {
 
   let response
 
+  console.log(cacheKey)
+
   if (cacheKey) {
     console.log('cacheKey', cacheKey.url)
     response = await cache.match(cacheKey)
   }
 
+  console.log(`Found response: ${!!response}`)
+
   if (!response) {
+    console.log('Revalidating')
     response = await fetchResponse()
     response = new Response(response.body, response)
 
     if (cacheKey) {
+      console.log(`Checking whether to cache response with code ${response.status}`)
       if (response.headers.has('Cache-Control') && response.status !== 403) {
+        console.log('Putting response into cache')
         // cache will respect response headers
         event.waitUntil(
           cache.put(cacheKey, response.clone()).catch((err) => {
